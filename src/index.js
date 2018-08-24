0a24475c0c8aab7cc8ffda358debb21e0393c72b
moti <moti.kor@gmail.com>
2018-08-24 02:22:51 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux'
+
+function reducer(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+const action = type => store.dispatch({type})
+
+store.subscribe(() => {
+  console.log('store updated')
+  console.log(store.getState())
+})
+
+action('INCREMENT')
+action('INCREMENT')
+action('DECREMENT')
+action('DECREMENT')
+action('BOOM')
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
